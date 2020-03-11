f13382a1f8f5c7e5116896cb93f173adab1fa5c7
lily <13516138165@163.com>
2020-03-11 14:23:14 +0800

    登录功能-发送请求

---
+++ src/api/user.js
@@ -0,0 +1,12 @@
+import request from '@/utils/request'
+
+/**
+ * 登录
+ * @param {*} 登录提交服务器数据 mobile,code
+ */
+export const login = ({ mobile, code }) => {
+  return request.post('/app/v1_0/authorizations', {
+    mobile,
+    code
+  })
+}
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,52 @@
 <template>
   <div>
-    <van-nav-bar
-      title="标题"
-      left-text="返回"
-      right-text="按钮"
-      left-arrow
-      @click-left="onClickLeft"
-      @click-right="onClickRight"
-    />
+    <!-- 导航头 -->
+    <van-nav-bar title="登录" />
+
+    <!-- 输入框 -->
+    <van-cell-group>
+      <van-field left-icon="contact" v-model="user.mobile" placeholder="请输入手机号" />
+      <van-field left-icon="smile-o" v-model="user.code" placeholder="请输入验证码">
+        <van-button size="small" slot="button">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <!-- 登录按钮 -->
+    <div class="login_btn">
+      <van-button class="btn" type="info" @click="handleLogin" >登录</van-button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { login } from '../api/user'
+export default {
+  name: 'Login',
+  data () {
+    return {
+      user: {
+        mobile: '13911111111',
+        code: '246810'
+      }
+    }
+  },
+  methods: {
+    async handleLogin () {
+      try {
+        const res = await login(this.user)
+        console.log(res)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="less" scoped>
+.login_btn {
+  padding: 20px 20px;
+  .btn {
+    width: 100%;
+  }
+}
 </style>
